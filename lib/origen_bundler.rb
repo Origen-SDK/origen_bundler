7362b43d0622f40c70ee6cef04a5c80a3d90165d
Stephen McGinty <stephen.mcginty@nxp.com>
2018-11-05 11:38:39 +0000

    Trying to get hook to register

--- lib/origen_bundler.rb
+++ lib/origen_bundler.rb
@@ -1,8 +1,4 @@
 require "origen_bundler/version"
 
 module OrigenBundler
-  Bundler::Plugin.add_hook('before-install-all') do |dependencies|
-    puts "Plugins is working!"
-    exit 0
-  end
 end
--- plugins.rb
+++ plugins.rb
@@ -1,1 +1,6 @@
 require 'origen_bundler'
+
+Bundler::Plugin.add_hook('before-install-all') do |dependencies|
+  puts "Plugin is working!"
+  exit 0
+end
